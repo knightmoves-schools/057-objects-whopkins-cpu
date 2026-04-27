818b21499adc6244c38237a058e93b0f20d1bb06
Weston Hopkins <whopkins@anamosa.k12.ia.us>
2026-04-27 17:17:03 +0000

    fourbigbasilisk

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+var school = {
+ name: 'blue balls';
+ 'average class size': 69;
+ mascot: 'hurtz';
+};
 
+var description = '${name} has an average class size of ${average class size} and their mascot is a ${mascot}';
+
+document.getElementById('result').innerHTML = description;
